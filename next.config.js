47dc6f1520c6692cd9f923617d8fa1d9570aa205
Elijah Pekson <elijah.pekson@twala.io>
2021-05-30 19:00:06 +0800

    Create Header component

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    images: {
+        domains: [
+            "cdn0.iconfinder.com",
+            "platform-lookaside.fbsbx.com",
+            "firebasestorage.googleapis.com"
+        ]
+    }
+}
