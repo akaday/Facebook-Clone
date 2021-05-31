14f93ac524638b29dbf902c40a37fddaf87a5189
Elijah Pekson <elijah.pekson@twala.io>
2021-05-31 20:33:30 +0800

    Firebase connection file

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+import 'firebase/storage';
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: "your-auth-domain",
+    projectId: "your-project-id",
+    storageBucket: "your-storage-bucket",
+    messagingSenderId: "your-messaging-sender-id",
+    appId: "your-app-id"
+};
+
+const app = !firebase.apps.length ? firebase.initializeApp(firebaseConfig) : firebase.app();
+
+const db = app.firestore();
+const storage = firebase.storage();
+
+export { db, storage };
